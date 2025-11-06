8d9ee661707a66cd2deeffbf8a48fb7b79e88320
Shoumik-20 <shoumikn@usc.edu>
2025-11-05 22:34:37 -0500

    Initial Commit

---
+++ JavaScript Crash Course for Beginners/main.js
@@ -0,0 +1,60 @@
+// // log a statement using console.log()
+// console.log('Hello from main.js')
+
+// // Variables
+
+// let age = 25
+// console.log(age)
+
+// // All const variables must be initialized and once initialized it cannot be reassigned new value
+// const salary = 80000
+// console.log(salary)
+
+// let sum = 0
+// sum = 5
+// console.log(sum)
+
+//// Data Type
+
+// String
+const name = "Shoumik";
+const language = "Javascript";
+
+// Number
+const total = 0;
+const PI = 3.14;
+
+// Boolean
+const isPrimaryNumber = true;
+const isNewUser = false;
+
+// Undefined
+let result;
+// console.log(result);
+const res = undefined;
+
+// If you want to assign a value not known use null
+const data = null;
+
+// Object
+
+// Object literal
+const person = {
+  firstName: "Bruce",
+  lastName: "Wayne",
+  age: 30,
+};
+
+console.log(person.firstName)
+
+// Array
+const oddNumbers = [1,3,5,7,9]
+
+console.log(oddNumbers[2])
+
+// Dynamically typed language
+
+let a = 10
+a = 'Vishwas'
+a = true
+console.log(a)
