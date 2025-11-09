e8ef88fd33eab59ffeeaff44510dd995ae3db531
Shoumik-20 <shoumikn@usc.edu>
2025-11-09 14:23:44 -0600

    Completed Beginner crash course

--- JavaScript Crash Course for Beginners/main.js
+++ JavaScript Crash Course for Beginners/main.js
@@ -16,45 +16,163 @@
 
 //// Data Type
 
-// String
-const name = "Shoumik";
-const language = "Javascript";
+// // String
+// const name = "Shoumik";
+// const language = "Javascript";
 
-// Number
-const total = 0;
-const PI = 3.14;
+// // Number
+// const total = 0;
+// const PI = 3.14;
 
-// Boolean
-const isPrimaryNumber = true;
-const isNewUser = false;
+// // Boolean
+// const isPrimaryNumber = true;
+// const isNewUser = false;
 
-// Undefined
-let result;
-// console.log(result);
-const res = undefined;
+// // Undefined
+// let result;
+// // console.log(result);
+// const res = undefined;
 
-// If you want to assign a value not known use null
-const data = null;
+// // If you want to assign a value not known use null
+// const data = null;
 
-// Object
+// // Object
 
-// Object literal
-const person = {
-  firstName: "Bruce",
-  lastName: "Wayne",
-  age: 30,
-};
+// // Object literal
+// const person = {
+//   firstName: "Bruce",
+//   lastName: "Wayne",
+//   age: 30,
+// };
 
-console.log(person.firstName)
+// console.log(person.firstName)
 
-// Array
-const oddNumbers = [1,3,5,7,9]
+// // Array
+// const oddNumbers = [1,3,5,7,9]
 
-console.log(oddNumbers[2])
+// console.log(oddNumbers[2])
 
 // Dynamically typed language
 
-let a = 10
-a = 'Vishwas'
-a = true
-console.log(a)
+// let a = 10
+// a = 'Vishwas'
+// a = true
+// console.log(a)
+
+// const isValid = false
+// console.log(!isValid)
+
+// Type conversion -> implicit conversion and explicit conversion
+
+// console.log(2+'3')
+// console.log(true+'3')
+// console.log('4' + '2')
+// console.log('Bruce' - 'Wayne')
+// console.log('5' - true)
+// console.log(5 + undefined)
+
+// console.log(Number('5'))
+// console.log(parseInt('5'))
+// console.log(String(5))
+// console.log((500).toString())
+// console.log(Boolean(10))
+
+// Equality
+
+// const var1 = 10
+// const var2 = '10'
+
+// console.log(var1 == var2)
+// console.log(var1 === var2)
+
+// Conditional Statements
+// const num = 0
+
+// if(num > 0) {
+//     console.log('Number is positive')
+// } else if (num < 0){
+//     console.log('Number is not positive')
+// }
+// else{
+//     console.log('Number is zero')
+// }
+
+// const color = 'red'
+
+// switch(color){
+//     case 'red':
+//         console.log('Color is red')
+//         break
+//     case 'blue':
+//         console.log('Color is blue')
+//         break
+//     case 'green':
+//         console.log('Color is green')
+//         break
+//     default:
+//         console.log('Not a color')
+// }
+
+// Looping
+
+// for (let i = 1; i <= 5; i ++){
+//     console.log('Iteration number' + i)
+// }
+
+// let i = 1
+// while(i <=5){
+//     console.log('Iteration while' + i)
+//     i++
+// }
+
+// let i = 6
+// do {
+//     console.log('Iteration while ' + i)
+//     i++
+// } while(i <=5)
+
+// const numArray = [1,2,3,4,5]
+
+// for (const num of numArray) {
+//     console.log('Iteration number ' + num)
+// }
+
+// Functions
+// function greet(userName) {                  // Parameters
+//     console.log('Good Morning ' + userName)
+// }
+
+// greet('Bruce') // Argument 
+// greet('Clark')
+
+// function add(a,b) {
+//     return a+b
+// }
+
+// const arrowSum = (a,b) => a+b
+
+// const addFive = (num) => num+5
+
+// const sum = arrowSum(5,10)
+// console.log(sum)
+
+// Scope 
+
+// Global Scope
+const myNum = 100
+
+// Block scope
+if(true){
+    const myName = 'Vishwas'
+    console.log(myName)
+    console.log(myNum)
+}
+
+// Function Scope
+function testFn(){
+    const myName = 'Batman'
+    console.log(myName)
+    console.log(myNum)
+}
+
+testFn()
